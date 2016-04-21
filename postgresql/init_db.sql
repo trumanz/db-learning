273b318814cc961ce57efa13b4f6ec7783a0efaf
Truman Zhou <Truman.Zhou@emc.com>
2016-04-21 19:13:45 +0800

    devl

---
+++ postgresql/init_db.sql
@@ -0,0 +1,51 @@
+
+-- User for slony-I
+CREATE ROLE slony WITH PASSWORD 'slony' LOGIN SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+ALTER ROLE slony
+  SET log_min_error_statement = 'debug';
+ALTER ROLE slony
+  SET log_min_messages = 'warning';
+
+--create Database 
+CREATE ROLE fly WITH PASSWORD 'fly';
+DROP DATABASE IF EXISTS fly;
+
+CREATE DATABASE fly  OWNER fly;
+
+
+\c fly;
+
+CREATE TABLE tasks(
+     id serial,
+     submit_time timestamp,
+     PRIMARY KEY(id)
+);
+CREATE TABLE  airports(
+     id text,
+     PRIMARY KEY(id)
+);
+
+CREATE TABLE flights (
+    task_id serial,
+    flight_id  text,
+    departure_airport text,
+    arrival_airport text,
+    departure_timestampe  timestamp,
+
+    FOREIGN KEY(task_id) REFERENCES tasks(id),
+    FOREIGN KEY(departure_airport) REFERENCES airports(id),
+    FOREIGN KEY(arrival_airport) REFERENCES airports(id),
+    PRIMARY KEY(task_id, flight_id)
+);
+CREATE INDEX task_id_idx ON flights(task_id);
+
+CREATE OR REPLACE FUNCTION public.load_flights_with_departure(
+     _departure_airport text,
+     _departure_timestampe_l timestamp,
+     _departure_timestampe_r timestamp)
+   RETURNS SETOF flights AS  $$
+   SELECT * FROM flights WHERE departure_airport = $1
+                 AND ($2 IS NULL OR departure_timestampe >= _departure_timestampe_l)
+                 AND ($3 IS NULL OR departure_timestampe <  _departure_timestampe_l)
+   $$ 
+   LANGUAGE sql STABLE;
--- postgresql/run.sh
+++ postgresql/run.sh
@@ -5,5 +5,10 @@ docker run --name=pg -h pg  -t -d -i  trumanz/postgresql  /start_pg.sh
 IP=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}'  pg)
 echo $IP
 
+ping -c 5 $IP
+export PG_HOST=$IP
+env PGPASSWORD=postgres psql -h $PG_HOST   -U postgres   -f  init_db.sql
+
+#python -m unittest discover -v  -s  ./test  -p test*.py
 docker rm -f  pg
 
